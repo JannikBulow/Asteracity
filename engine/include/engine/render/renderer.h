f8bafcd77fe8962a84f4062de54ebc7311ed6a24
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-09-08 21:34:08 +0200

    feat(renderer): remove explicit color parameter for drawSprite and use tint()

--- engine/include/engine/render/renderer.h
+++ engine/include/engine/render/renderer.h
@@ -21,7 +21,7 @@ namespace engine {
 
         void drawTexture(const Texture& texture, math::Vec2 position, math::Vec2 size, math::Color color = math::Color::White, float rotation = 0.0f, math::Rect uv = {0, 1, 1, 0});
 
-        void drawSprite(const Sprite& sprite, math::Vec2 position, math::Vec2 size, math::Color color = math::Color::White, float rotation = 0.0f);
+        void drawSprite(const Sprite& sprite, math::Vec2 position, math::Vec2 size, float rotation = 0.0f);
 
         void drawText(const Font& font, const std::string& text, math::Vec2 position, float fontSize, math::Color color, bool centerOrigin = false);
         void drawText(const Font& font, const std::string& text, math::Vec2 position, float fontSize, float spacing, float textLineSpacing, math::Color color, bool centerOrigin = false);
