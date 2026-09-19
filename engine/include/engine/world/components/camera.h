46731845fe2f8081b80cf9f962aee875ad257763
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-09-19 20:45:52 +0200

    feat(component): add camera component

---
+++ engine/include/engine/world/components/camera.h
@@ -0,0 +1,14 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#ifndef ASTERACITY_ENGINE_WORLD_COMPONENTS_CAMERA_H
+#define ASTERACITY_ENGINE_WORLD_COMPONENTS_CAMERA_H
+
+#include "engine/render/camera.h"
+
+namespace engine {
+    struct CameraComponent {
+        CameraHandle camera;
+    };
+}
+
+#endif //ASTERACITY_ENGINE_WORLD_COMPONENTS_CAMERA_H
