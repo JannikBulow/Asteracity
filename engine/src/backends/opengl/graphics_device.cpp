eb5f2077a82e19f7af9c8f15d1d77b16f4cf64ca
jannik <dodoakmishot@gmail.com>
2026-09-25 10:38:06 +0200

    feat(opengl-graphics-device): add default wrap parameters to textures

--- engine/src/backends/opengl/graphics_device.cpp
+++ engine/src/backends/opengl/graphics_device.cpp
@@ -284,6 +284,8 @@ namespace backend {
         glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, image.width, image.height, 0, format, GL_UNSIGNED_BYTE, image.pixels);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
         return mTextures.create(texture);
     }
